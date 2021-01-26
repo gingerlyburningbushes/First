bdfbecb71d5b946fd480c8ab3a35b6010538e9f7
john wu <john.z.wu@gmail.com>
2021-01-26 15:24:03 -0500

    first java main program

---
+++ MyFirstApplication/src/main/java/foo/Foo.java
@@ -0,0 +1,10 @@
+package foo;
+
+public class Foo {
+    public static void main(String[] arg) {
+        String hi = "Hello This is my first java main program";
+        System.out.println(hi);
+    }
+
+}
+
